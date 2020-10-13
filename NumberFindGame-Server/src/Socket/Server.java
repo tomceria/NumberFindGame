890cfe251c6e28a6758a14552452776fd60b23b7
Lưu Minh Hoàng <ceriagame@gmail.com>
2020-10-14 03:21:41 +0700

    HashMap clientConnections

--- NumberFindGame-Server/src/Socket/Server.java
+++ NumberFindGame-Server/src/Socket/Server.java
@@ -1,14 +1,16 @@
 package Socket;
 
-import sun.rmi.runtime.Log;
+import com.mysql.cj.xdevapi.Client;
 
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.UUID;
 
 public class Server {
+    static HashMap<UUID, ClientHandler> clientConnections = new HashMap<UUID, ClientHandler>();
+
     // TODO: Placeholder - REMOOOOOVE
     public static void main(String[] args) {
         try {
@@ -17,6 +19,11 @@ public class Server {
                 Socket client = server.accept();
                 ClientHandler clientHandler = new ClientHandler(client);
                 clientHandler.start();
+                clientConnections.put(UUID.randomUUID(), clientHandler);
+                System.out.println("Current connections: ");
+                for (UUID key : clientConnections.keySet()) {
+                    System.out.println(key + ": " + clientConnections.get(key).client.getInetAddress().toString());
+                }
             }
 
         } catch (IOException e) {
