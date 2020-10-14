f24dee45690707952a34214cdbd67baa932d6d94
Lưu Minh Hoàng <ceriagame@gmail.com>
2020-10-14 16:54:02 +0700

    LevelNodeButton: Hover

--- NumberFindGame-Client/src/GUI/Components/LevelNodeButton.java
+++ NumberFindGame-Client/src/GUI/Components/LevelNodeButton.java
@@ -5,9 +5,13 @@ import Models.MatchPlayer;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class LevelNodeButton extends JButton implements IComponent {
+    boolean isPicked = false;
     final int SIZE = 30;
+    Color assignedColor;
 
     public LevelNodeButton() {
         super();
@@ -25,17 +29,32 @@ public class LevelNodeButton extends JButton implements IComponent {
         this.setSize(SIZE, SIZE);
         this.setHorizontalAlignment(SwingConstants.CENTER);
         this.setMargin(new Insets(0, SIZE * -1, 0, SIZE * -1));
-        this.setBorderPainted(false);
         this.setFocusPainted(false);
-        this.setContentAreaFilled(false);
-        this.setBorder(null);
+        this.setBorderPainted(true);
+        this.setBorder(BorderFactory.createMatteBorder(0, 0, 0, 0, Color.decode("#191E80")));
         this.setForeground(Color.WHITE);
+        this.setBackground(Color.decode("#1A237E"));
         this.setFont(new Font("SansSerif", Font.PLAIN, 14));
+
+        // Hover
+        this.addMouseListener(new MouseAdapter() {
+            public void mouseEntered(MouseEvent evt) {
+                setBackground(Color.decode("#2E41EC"));
+            }
+
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                Color ogColor = isPicked ? assignedColor : Color.decode("#1A237E");
+                setBackground(ogColor);
+            }
+        });
     }
 
     public void setPicked(MatchPlayer matchPlayer) {
         this.setOpaque(true);
         this.setBackground(matchPlayer.getUiColor());
         this.setForeground(Color.BLACK);
+        this.isPicked = true;
+        this.assignedColor = matchPlayer.getUiColor();
+        System.out.println("PICKED");
     }
 }
