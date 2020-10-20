b9a8074d84c5fad31f1719906926d4f9823beca5
Lưu Minh Hoàng <ceriagame@gmail.com>
2020-10-20 21:14:32 +0700

    DTO/MatchConfig

---
+++ NumberFindGame-Server/src/dto/MatchConfig.java
@@ -0,0 +1,31 @@
+package dto;
+
+public class MatchConfig {
+    private int numberQty;
+    private int time;
+    private int maxPlayer;
+
+    public int getNumberQty() {
+        return numberQty;
+    }
+
+    public void setNumberQty(int numberQty) {
+        this.numberQty = numberQty;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public int getMaxPlayer() {
+        return maxPlayer;
+    }
+
+    public void setMaxPlayer(int maxPlayer) {
+        this.maxPlayer = maxPlayer;
+    }
+}
