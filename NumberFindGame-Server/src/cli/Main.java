c02d71005a26c9b2b17e780d5a2632e580d5bce1
JaySGH <huysgh94@gmail.com>
2020-10-27 13:23:47 +0700

    clean up

--- NumberFindGame-Server/src/cli/Main.java
+++ NumberFindGame-Server/src/cli/Main.java
@@ -16,40 +16,40 @@ public class Main {
 
 	public static void main(String[] args) throws NumberValidException {
 
-		JsonHelper jsonHelper = new JsonHelper();
-		MatchConfig matchConfig =  jsonHelper.readConfig();
-		matchConfig.setNumberQty(550);
-		jsonHelper.saveConfig(matchConfig);
+//		JsonHelper jsonHelper = new JsonHelper();
+//		MatchConfig matchConfig =  jsonHelper.readConfig();
+//		matchConfig.setNumberQty(550);
+//		jsonHelper.saveConfig(matchConfig);
 
 		// PlayerBUS playerBus = new PlayerBUS();
 		// System.out.print(playerBus.login("saidan00", "123"));
 
 		// Start server
-//		server = new GameServer(54321);
-//		server.listen();
-//
-//		do {
-//			String command = scan.nextLine();
-//			switch (command) {
-//				case "list": {
-//					System.out.println("Current connections: ");
-//					for (UUID key : server.getClientManager().getClientConnections().keySet()) {
-//						System.out.println(key + ": " +
-//								server.getClientManager().getClientConnections()
-//										.get(key)
-//										.getClient()
-//										.getInetAddress()
-//										.toString());
-//					}
-//					break;
-//				}
-//				case "exit": {
-//					server.halt();
-//					isCommanding = false;
-//					break;
-//				}
-//			}
-//		} while (isCommanding);
+		server = new GameServer(54321);
+		server.listen();
+
+		do {
+			String command = scan.nextLine();
+			switch (command) {
+				case "list": {
+					System.out.println("Current connections: ");
+					for (UUID key : server.getClientManager().getClientConnections().keySet()) {
+						System.out.println(key + ": " +
+								server.getClientManager().getClientConnections()
+										.get(key)
+										.getClient()
+										.getInetAddress()
+										.toString());
+					}
+					break;
+				}
+				case "exit": {
+					server.halt();
+					isCommanding = false;
+					break;
+				}
+			}
+		} while (isCommanding);
 	}
 
 }
