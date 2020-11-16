861919334d44bfc548dd35d87a1d2cdf0e430ea8
Anh Ngan <anhngan272@gmail.com>
2020-11-16 09:07:15 +0700

    remove readFile

--- NumberFindGame-Server/src/Socket/Logging.java
+++ NumberFindGame-Server/src/Socket/Logging.java
@@ -1,12 +1,11 @@
 package Socket;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 import java.time.format.DateTimeFormatter;
+
 import java.time.LocalDateTime;
 
 public class Logging {
@@ -14,39 +13,15 @@ public class Logging {
 	static String filePath = new File("").getAbsolutePath() + "\\log.txt";
 	static File file = new File(filePath);
 
-	public static void readFile() {
-		BufferedReader reader = null;
-		String result = "";
-		String currentLine;
-
-		try {
-			reader = new BufferedReader(new FileReader(file));
-
-			while ((currentLine = reader.readLine()) != null) {
-				System.out.println(currentLine);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (reader != null) {
-					reader.close();
-				}
-			} catch (IOException ex) {
-				ex.printStackTrace();
-			}
-		}
-
-	}
-
 	public static void writeFile(String s) {
 		FileWriter fw = null;
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"); //16-11-2020 00:39:47
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"); // 16-11-2020 00:39:47
 		LocalDateTime now = LocalDateTime.now();
-		System.out.println(dtf.format(now));
 		try {
+			if (!file.exists()) {
+				file.createNewFile();
+			}
 			fw = new FileWriter(file, true);
-
 			fw.write(dtf.format(now) + "  " + s);
 			fw.write("\n");
 
